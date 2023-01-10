13ba04bb7ad7a84e3cc69fdad47d27fc1d3ca513
tojauch <jauch@eit.uni-kl.de>
2023-01-10 16:55:32 +0100

    removed unsafe_miss signal to prevent stores from retrying addr translation

--- src/main/scala/lsu/lsu.scala
+++ src/main/scala/lsu/lsu.scala
@@ -176,7 +176,7 @@ class LDQEntry(implicit p: Parameters) extends BoomBundle()(p)
 
   val executed            = Bool() // load sent to memory, reset by NACKs
   val succeeded           = Bool()
-	val failure             = Bool() // added by mofadiheh for meltdown fix
+  val failure             = Bool() // added by mofadiheh for meltdown fix
   val order_fail          = Bool()
   val observed            = Bool()
 
@@ -198,7 +198,6 @@ class STQEntry(implicit p: Parameters) extends BoomBundle()(p)
 
   val committed           = Bool() // committed by ROB
   val succeeded           = Bool() // D$ has ack'd this, we don't need to maintain this anymore
-	val unsafe_miss					= Bool() // added by mofadiheh - STT bug fix
 
   val debug_wb_data       = UInt(xLen.W)
 }
@@ -332,7 +331,6 @@ class LSU(implicit p: Parameters, edge: TLEdgeOut) extends BoomModule()(p)
       stq(st_enq_idx).bits.data.valid := false.B
       stq(st_enq_idx).bits.committed  := false.B
       stq(st_enq_idx).bits.succeeded  := false.B
-      stq(st_enq_idx).bits.unsafe_miss:= false.B // added by mofadiheh STT bug fix
 
       assert (st_enq_idx === io.core.dis_uops(w).bits.stq_idx, "[lsu] mismatch enq store tag.")
       assert (!stq(st_enq_idx).valid, "[lsu] Enqueuing uop is overwriting stq entries")
@@ -497,7 +495,7 @@ class LSU(implicit p: Parameters, edge: TLEdgeOut) extends BoomModule()(p)
                                ( stq_retry_e.valid                            &&
                                  stq_retry_e.bits.addr.valid                  &&
                                  stq_retry_e.bits.addr_is_virtual             &&
-															   (!stq_retry_e.bits.unsafe_miss || !stq_retry_e.bits.uop.taint ) && // added by mofadiheh STT bug fix
+															   !stq_retry_e.bits.uop.taint                  && // added by mofadiheh STT bug fix fix
                                  (w == memWidth-1).B                          &&
                                  RegNext(dtlb.io.miss_rdy)                    &&
                                  !(widthMap(i => (i != w).B               &&
@@ -951,7 +949,6 @@ class LSU(implicit p: Parameters, edge: TLEdgeOut) extends BoomModule()(p)
       stq(stq_idx).bits.addr.bits  := Mux(exe_tlb_miss(w), exe_tlb_vaddr(w), exe_tlb_paddr(w))
       stq(stq_idx).bits.uop.pdst   := exe_tlb_uop(w).pdst // Needed for AMOs
       stq(stq_idx).bits.addr_is_virtual := exe_tlb_miss(w)
-      stq(stq_idx).bits.unsafe_miss     := exe_tlb_miss(w) & exe_tlb_uop(w).taint // added by mofadiheh STT bug fix
 
       assert(!(will_fire_sta_incoming(w) && stq_incoming_e(w).bits.addr.valid),
         "[lsu] Incoming store is overwriting a valid address")
