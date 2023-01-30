fbcb4a43c3f630332126b1a78542038538bc980e
Philipp Schmitz <schmitz@eit.uni-kl.de>
2023-01-30 16:56:28 +0100

    Remove killing fdivsqrt instructions that are already inside the pipeline

--- src/main/scala/exu/execution-units/fdiv.scala
+++ src/main/scala/exu/execution-units/fdiv.scala
@@ -24,8 +24,8 @@ import freechips.rocketchip.tile.HasFPUParameters
 import freechips.rocketchip.util.uintToBitPat
 
 /**
- * Decoder for FPU divide and square root signals
- */
+  * Decoder for FPU divide and square root signals
+  */
 class UOPCodeFDivDecoder(implicit p: Parameters) extends BoomModule
   with HasFPUParameters
 {
@@ -60,27 +60,27 @@ class UOPCodeFDivDecoder(implicit p: Parameters) extends BoomModule
 
   val s = io.sigs
   val sigs = Seq(s.ldst, s.wen, s.ren1, s.ren2, s.ren3, s.swap12,
-                 s.swap23, s.typeTagIn, s.typeTagOut, s.fromint, s.toint, s.fastpipe, s.fma,
-                 s.div, s.sqrt, s.wflags)
+    s.swap23, s.typeTagIn, s.typeTagOut, s.fromint, s.toint, s.fastpipe, s.fma,
+    s.div, s.sqrt, s.wflags)
   sigs zip decoder map {case(s,d) => s := d}
 }
 
 /**
- * fdiv/fsqrt is douple-precision. Must upconvert inputs and downconvert outputs
- * as necessary.  Must wait till killed uop finishes before we're ready again.
- * fdiv/fsqrt unit uses an unstable FIFO interface, and thus we must spend a
- * cycle buffering up an uop to provide slack between the issue queue and the
- * fdiv/fsqrt unit.  FDivUnit inherents directly from FunctionalUnit, because
- * UnpipelinedFunctionalUnit can only handle 1 inflight uop, whereas FDivUnit
- * contains up to 2 inflight uops due to the need to buffer the input as the
- * fdiv unit uses an unstable FIFO interface.
- * TODO extend UnpipelinedFunctionalUnit to handle a >1 uops inflight.
- *
- * @param isPipelined is the functional unit pipelined
- * @param numStages number of stages for the functional unit
- * @param numBypassStages number of bypass stages
- * @param dataWidth width of the data out of the functional unit
- */
+  * fdiv/fsqrt is douple-precision. Must upconvert inputs and downconvert outputs
+  * as necessary.  Must wait till killed uop finishes before we're ready again.
+  * fdiv/fsqrt unit uses an unstable FIFO interface, and thus we must spend a
+  * cycle buffering up an uop to provide slack between the issue queue and the
+  * fdiv/fsqrt unit.  FDivUnit inherents directly from FunctionalUnit, because
+  * UnpipelinedFunctionalUnit can only handle 1 inflight uop, whereas FDivUnit
+  * contains up to 2 inflight uops due to the need to buffer the input as the
+  * fdiv unit uses an unstable FIFO interface.
+  * TODO extend UnpipelinedFunctionalUnit to handle a >1 uops inflight.
+  *
+  * @param isPipelined is the functional unit pipelined
+  * @param numStages number of stages for the functional unit
+  * @param numBypassStages number of bypass stages
+  * @param dataWidth width of the data out of the functional unit
+  */
 class FDivSqrtUnit(implicit p: Parameters)
   extends FunctionalUnit(
     isPipelined = false,
@@ -88,7 +88,7 @@ class FDivSqrtUnit(implicit p: Parameters)
     numBypassStages = 0,
     dataWidth = 65,
     needsFcsr = true)
-  with tile.HasFPUParameters
+    with tile.HasFPUParameters
 {
   //--------------------------------------
   // buffer inputs and upconvert as needed
@@ -104,9 +104,9 @@ class FDivSqrtUnit(implicit p: Parameters)
   // handle branch kill on queued entry
   r_buffer_val := !IsKilledByBranch(io.brupdate, r_buffer_req.uop, 4.U) && !io.req.bits.kill && r_buffer_val
 
-	// the taint should be updated too
-	//added by mofadiheh for taint
-	r_buffer_req.uop.taint   := GetNewImplicitTaint(io.brupdate, r_buffer_req.uop)
+  // the taint should be updated too
+  //added by mofadiheh for taint
+  r_buffer_req.uop.taint   := GetNewImplicitTaint(io.brupdate, r_buffer_req.uop)
 
   r_buffer_req.uop.br_mask := GetNewBrMask(io.brupdate, r_buffer_req.uop)
 
@@ -128,9 +128,9 @@ class FDivSqrtUnit(implicit p: Parameters)
     r_buffer_req := io.req.bits
     r_buffer_req.uop.br_mask := GetNewBrMask(io.brupdate, io.req.bits.uop)
 
-	// the taint should be updated too
-	//added by mofadiheh for taint
-	r_buffer_req.uop.taint   := GetNewImplicitTaint(io.brupdate, io.req.bits.uop)
+    // the taint should be updated too
+    //added by mofadiheh for taint
+    r_buffer_req.uop.taint   := GetNewImplicitTaint(io.brupdate, io.req.bits.uop)
 
     r_buffer_fin <> fdiv_decoder.io.sigs
 
@@ -162,9 +162,9 @@ class FDivSqrtUnit(implicit p: Parameters)
 
   val may_fire_input =
     r_buffer_val &&
-    (r_buffer_fin.div || r_buffer_fin.sqrt) &&
-    !r_divsqrt_val &&
-    output_buffer_available
+      (r_buffer_fin.div || r_buffer_fin.sqrt) &&
+      !r_divsqrt_val &&
+      output_buffer_available
 
   val divsqrt_ready = Mux(divsqrt.io.sqrtOp, divsqrt.io.inReady_sqrt, divsqrt.io.inReady_div)
   divsqrt.io.inValid := may_fire_input // must be setup early
@@ -174,7 +174,7 @@ class FDivSqrtUnit(implicit p: Parameters)
   divsqrt.io.roundingMode := r_buffer_fin.rm
   divsqrt.io.detectTininess := DontCare
 
-  r_divsqrt_killed := r_divsqrt_killed || IsKilledByBranch(io.brupdate, r_divsqrt_uop, 4.U) || io.req.bits.kill
+  r_divsqrt_killed := r_divsqrt_killed || IsKilledByBranch(io.brupdate, r_divsqrt_uop) || io.req.bits.kill
 
   // the taint should be updated too
   //added by mofadiheh for taint
@@ -189,12 +189,12 @@ class FDivSqrtUnit(implicit p: Parameters)
     r_divsqrt_val := true.B
     r_divsqrt_fin := r_buffer_fin
     r_divsqrt_uop := r_buffer_req.uop
-    r_divsqrt_killed := IsKilledByBranch(io.brupdate, r_buffer_req.uop, 4.U) || io.req.bits.kill
+    r_divsqrt_killed := IsKilledByBranch(io.brupdate, r_buffer_req.uop) || io.req.bits.kill
     r_divsqrt_uop.br_mask := GetNewBrMask(io.brupdate, r_buffer_req.uop)
 
-	// the taint should be updated too
-	//added by mofadiheh for taint
-	r_divsqrt_uop.taint   := GetNewImplicitTaint(io.brupdate, r_buffer_req.uop)
+    // the taint should be updated too
+    //added by mofadiheh for taint
+    r_divsqrt_uop.taint   := GetNewImplicitTaint(io.brupdate, r_buffer_req.uop)
 
   }
 
@@ -221,13 +221,13 @@ class FDivSqrtUnit(implicit p: Parameters)
   when (divsqrt.io.outValid_div || divsqrt.io.outValid_sqrt) {
     r_divsqrt_val := false.B
 
-    r_out_val := !r_divsqrt_killed && !IsKilledByBranch(io.brupdate, r_divsqrt_uop, 4.U) && !io.req.bits.kill
+    r_out_val := !r_divsqrt_killed && !IsKilledByBranch(io.brupdate, r_divsqrt_uop) && !io.req.bits.kill
     r_out_uop := r_divsqrt_uop
     r_out_uop.br_mask := GetNewBrMask(io.brupdate, r_divsqrt_uop)
 
-	// the taint should be updated too
-	//added by mofadiheh for taint
-	r_out_uop.taint   := GetNewImplicitTaint(io.brupdate, r_divsqrt_uop)
+    // the taint should be updated too
+    //added by mofadiheh for taint
+    r_out_uop.taint   := GetNewImplicitTaint(io.brupdate, r_divsqrt_uop)
 
     r_out_wdata_double := sanitizeNaN(divsqrt.io.out, tile.FType.D)
     r_out_flags_double := divsqrt.io.exceptionFlags
